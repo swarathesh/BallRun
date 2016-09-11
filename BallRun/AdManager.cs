aa6da67d3fbe00708cbdeb0fb602fa4f21d8bbeb
salyert <swarathesh60@gmail.com>
2016-09-11 09:31:45 +0530

    First Commit for

---
+++ BallRun/AdManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AdManager : MonoBehaviour {
+
+	public static AdManager instance;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		AppLovin.InitializeSdk ();
+		AppLovin.PreloadInterstitial ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowInterstitialAd(){
+
+		Debug.Log ("Ads Showing");
+
+		if (AppLovin.HasPreloadedInterstitial ()) {
+			AppLovin.ShowInterstitial ();
+		}
+	}
+}
---
+++ BallRun/Assets/Scripts/ScoreSetterGetter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	public class ScoreSetterGetter
+	{
+		public ScoreSetterGetter ()
+		{
+		}
+	}
+}
+
